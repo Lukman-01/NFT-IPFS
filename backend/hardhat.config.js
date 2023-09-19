73a9d45c79d3c3525fd1a8e4f72d179f32b09c0f
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-09-19 04:36:01 +0100

    add check

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -4,6 +4,10 @@ require("dotenv").config({ path: ".env" });
 const ALCHEMY_HTTP_URL = process.env.ALCHEMY_HTTP_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
+if (!ALCHEMY_HTTP_URL || !PRIVATE_KEY) {
+  throw new Error("ALCHEMY_HTTP_URL and PRIVATE_KEY must be defined in your .env file.");
+}
+
 module.exports = {
   solidity: "0.8.4",
   networks: {
